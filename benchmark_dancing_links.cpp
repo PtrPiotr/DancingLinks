89014295511c8b6f4e79df6eb321f4cc9875e97f
Piotr Malesa <piotr.malesa@gmail.com>
2025-06-12 21:01:33 +0200

    added benchmark toolkit

---
+++ benchmark_dancing_links.cpp
@@ -0,0 +1,41 @@
+#include <benchmark/benchmark.h>
+#include "sudoku.h"
+
+
+int SolveSudoku(unsigned side) {
+    auto solver = sudoku::CreateEmptySudokuSolver(side);
+    auto solution = solver->Solve();
+    return 0;
+    
+}
+
+static void BM_DancingLinksSolverForSudoku25(benchmark::State& state) {
+  for (auto _ : state) 
+  {
+    SolveSudoku(25);
+  }
+}
+
+static void BM_DancingLinksSolverForSudoku36(benchmark::State& state) {
+  for (auto _ : state) 
+  {
+    SolveSudoku(36);
+  }
+}
+
+static void BM_DancingLinksSolverForSudoku49(benchmark::State& state) {
+  for (auto _ : state) 
+  {
+    SolveSudoku(49);
+  }
+}
+
+// Register the function as a benchmark
+BENCHMARK(BM_DancingLinksSolverForSudoku25);
+
+BENCHMARK(BM_DancingLinksSolverForSudoku36);
+
+BENCHMARK(BM_DancingLinksSolverForSudoku49);
+
+// Run the benchmark
+BENCHMARK_MAIN();
